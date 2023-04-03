5ab561da1a2c83a78b31570329a3bd901d195ea6
bealecs <cliftonscottbeale@gmail.com>
2023-04-03 14:14:21 -0500

    listed steps as comments to outline objectives

--- Portfolio/issues/Issue6.js
+++ Portfolio/issues/Issue6.js
@@ -1,20 +1,24 @@
+    //Step 1: Assign an array of 5 random numbers to a variable
     var numArray = [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)];
     
+    //Step 2: Assign a number to a variable and call it counter
     let counter = 0;
     
+    //Step 3: Use a for loop to iterate through the array and add the counter to each item in the array
     for(let i = 0 ; i < numArray.length; i++) {
-        counter++; //increment counter variable with each array index
-        const oldValue = numArray[i]; //value from the random number generator in the array index before adding counter variable
+        //Step 4: On each iteration add 1 to the counter
+        counter++; 
+        //value from the random number generator in the array index before adding counter variable
+        const oldValue = numArray[i]; 
+        //value from the random number generator in the array index after adding counter variable
+        const newValue = numArray[i] + counter; 
         console.log(`The updated value before counting is: ${oldValue}`);
-        const newValue = numArray[i] + counter; //value from the random number generator in the array index after adding counter variable
         console.log(`The updated value after counting is: ${newValue}`);
     }
 
-
+    //Step 5: Define a function that takes an array as an argument and uses a while loop to console.log whether each item in the array is even or odd
     function issue6(arr) {
-
         let i = 0; //index variable
-
         while( i < arr.length) { //loop continues while there are indexes in the given array parameter
             if(arr[i] % 2 === 0) { //modulus operator to determine if the number has a remainder or not when divided by 2
                 console.log("This value is even");
@@ -25,17 +29,20 @@
         }
     }
 
-    issue6(numArray); //call to the function passing numArray (array with 5 randomly generated numbers) as the argument
+    //Step 6: Call the above function with the previously assigned array variable as a parameter
+    issue6(numArray); 
 
-    //Call a 2nd time with 5 more random numbers *Did not save these numbers to a variable this time
+    //Step 7: Call the above function with a new array with 5 random numbers
     issue6([Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)]);
 
+    //Step 8: Delete the third item of the saved array
     const newNumberArray = numArray.splice(2, 1);
 
-    //adds string to beginning of array
+    //Step 9: Add a string to the front of the saved array
     numArray.unshift("Hello World");
 
-    //adds string to end of array
+    //Step 10: Add a string to the end of the saved array
     numArray.push("The End");
 
+    //Step 11: console.log the saved array
     console.log(numArray);
