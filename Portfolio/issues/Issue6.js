82fae878fca4f264f5c2c1a63fd7ca363a003bbc
bealecs <cliftonscottbeale@gmail.com>
2023-04-04 20:06:35 -0500

    changes made to comments left on issue 6

--- Portfolio/issues/Issue6.js
+++ Portfolio/issues/Issue6.js
@@ -1,13 +1,13 @@
     //Step 1: Assign an array of 5 random numbers to a variable
-    let numArray = new Array(5)
-    numArray = numArray.fill(0).map(() => Math.floor(Math.random() * 100));
+    let numArray = new Array(5).fill(Math.floor(Math.random() * 100));
     
     //Step 2: Assign a number to a variable and call it counter
     //Step 3: Use a for loop to iterate through the array and add the counter to each item in the array
     //Step 4: On each iteration add 1 to the counter
     for(let counter = 0 ; counter < numArray.length; counter++) {
         const newValue = numArray[counter] + counter; 
-        console.log(`Old Value: ${numArray[counter]} | The updated value after counting is: ${newValue}`);
+        console.log(`Old Value: ${numArray[counter]} \n
+         The updated value after counting is: ${newValue}`);
     }
 
     //Step 5: Define a function that takes an array as an argument and uses a while loop to console.log whether each item in the array is even or odd
@@ -27,8 +27,7 @@
     issue6(numArray); 
 
     //Step 7: Call the above function with a new array with 5 random numbers
-    issue6([Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)]);
-
+    issue6(numArray);
     //Step 8: Delete the third item of the saved array
     const newNumberArray = numArray.splice(2, 1);
 
@@ -39,5 +38,4 @@
     numArray.push("The End");
 
     //Step 11: console.log the saved array
-    console.log(newNumberArray);
-    console.log(numArray);
+    console.log({ newNumberArray, numArray});
