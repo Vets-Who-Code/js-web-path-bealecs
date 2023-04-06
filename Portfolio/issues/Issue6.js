86c5c80cda6e0765bfb5986a13994f9109911c0e
bealecs <cliftonscottbeale@gmail.com>
2023-04-06 18:02:03 -0500

    change to issue 6 bc it wasnt working

--- Portfolio/issues/Issue6.js
+++ Portfolio/issues/Issue6.js
@@ -1,15 +1,19 @@
     //Step 1: Assign an array of 5 random numbers to a variable
-    const genRandArr = () => new Array(5).fill(Math.floor(Math.random() * 100));
+    const genRandArr = () => {
+       let numbers = [];
+       for(let i = 0; i < 5; i++) {
+        numbers.push(Math.floor(Math.random() * 100));
+       }
+       return numbers;
+    };
+    const numArray = genRandArr();
 
-    //Step 2: Assign a number to a variable and call it counter
-    //Step 3: Use a for loop to iterate through the array and add the counter to each item in the array
-    //Step 4: On each iteration add 1 to the counter
     for(let counter = 0 ; counter < numArray.length; counter++) {
         const newValue = numArray[counter] + counter; 
         console.log(`Old Value: ${numArray[counter]} \n
          The updated value after counting is: ${newValue}`);
     }
-
+    genRandArr();
     //Step 5: Define a function that takes an array as an argument and uses a while loop to console.log whether each item in the array is even or odd
     function issue6(arr) {
         let i = 0; //index variable
@@ -24,12 +28,12 @@
     }
 
     //Step 6: Call the above function with the previously assigned array variable as a parameter
-    issue6(getRandArr()); 
+    issue6(genRandArr()); 
 
     //Step 7: Call the above function with a new array with 5 random numbers
-    issue6(getRandArr());
+    issue6(genRandArr());
     //Step 8: Delete the third item of the saved array
-    const newNumberArray = getRandArr().splice(2, 1);
+    const newNumberArray = genRandArr().splice(2, 1);
 
     //Step 9: Add a string to the front of the saved array
     numArray.unshift("Hello World");
