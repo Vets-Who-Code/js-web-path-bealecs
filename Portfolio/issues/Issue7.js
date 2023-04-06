cabf5028f4fd9861db6ce48bb07b55d9c6785032
bealecs <cliftonscottbeale@gmail.com>
2023-04-06 18:22:04 -0500

    changes to issue 7 as requested by schuster R4C

--- Portfolio/issues/Issue7.js
+++ Portfolio/issues/Issue7.js
@@ -3,7 +3,7 @@ const body = document.getElementById('issue7');
 
 function createElement(element, text, name) { //function to create an element given its parameters of element type, text content, and id-name
   const created = document.createElement(element); //variable for the newly created element
-  created.setAttribute("id", name); //sets created elements id = name parameter
+  created.setAttribute("class", name); //sets created elements id = name parameter
   created.textContent = text; //sets text content to text parameter
   body.appendChild(created); //appends to html <body>
   return created;
@@ -17,7 +17,7 @@ function addList() { //function to create list item, used with button for adding
 }
 
 function removeList() { //function to remove list item, used with button for removing list item
-  const listItem = document.getElementById("list-item");
+  const listItem = document.querySelector(".list-item");
   if (listItem) { //ensures that there is a list item to remove
     body.removeChild(listItem);
   }
@@ -26,8 +26,8 @@ function removeList() { //function to remove list item, used with button for rem
 createElement('button', 'Add List Item', 'add-button'); //creates add button using function call
 createElement('button', 'Remove List Item', 'remove-button'); //creates remove button using function call
 
-const addButton = document.getElementById("add-button"); //assigns add button to addButton variable
-const removeButton = document.getElementById("remove-button"); //assigns remove button to removeButton variable
+const addButton = document.querySelector(".add-button"); //assigns add button to addButton variable
+const removeButton = document.querySelector(".remove-button"); //assigns remove button to removeButton variable
 
 addButton.addEventListener('click', addList); //adding event listener for on click with function for adding a list using the add list button
 removeButton.addEventListener('click', removeList); //adding event listener for on click with function for removing a list using the removing list button
