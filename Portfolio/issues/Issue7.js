a52706c4c7003501b201be8dc50fd92204ba0990
bealecs <cliftonscottbeale@gmail.com>
2023-04-10 16:00:10 -0500

    a lot of styling changes / still needs work

--- Portfolio/issues/Issue7.js
+++ Portfolio/issues/Issue7.js
@@ -1,16 +1,16 @@
 //Step 1: Select the <body> element
-const body = document.getElementById('issue7');
+const issue7Body = document.getElementById('issue7');
 
-function createElement(element, text, name) { //function to create an element given its parameters of element type, text content, and id-name
+function createElement7(element, text, name) { //function to create an element given its parameters of element type, text content, and id-name
   const created = document.createElement(element); //variable for the newly created element
   created.setAttribute("class", name); //sets created elements id = name parameter
   created.textContent = text; //sets text content to text parameter
-  body.appendChild(created); //appends to html <body>
+  issue7Body.appendChild(created); //appends to html <body>
   return created;
 }
 
 function addList() { //function to create list item, used with button for adding list item
-    const listItem = createElement('li', 'List item', 'list-item');
+    const listItem = createElement7('li', 'List item', 'list-item');
     listItem.addEventListener('click', () => {
       listItem.classList.toggle('clicked'); //when clicked, list items have a background color of red, 1.5rem font size and white font color
     });
@@ -19,12 +19,12 @@ function addList() { //function to create list item, used with button for adding
 function removeList() { //function to remove list item, used with button for removing list item
   const listItem = document.querySelector(".list-item");
   if (listItem) { //ensures that there is a list item to remove
-    body.removeChild(listItem);
+    issue7Body.removeChild(listItem);
   }
 }
 
-createElement('button', 'Add List Item', 'add-button'); //creates add button using function call
-createElement('button', 'Remove List Item', 'remove-button'); //creates remove button using function call
+createElement7('button', 'Add List Item', 'add-button'); //creates add button using function call
+createElement7('button', 'Remove List Item', 'remove-button'); //creates remove button using function call
 
 const addButton = document.querySelector(".add-button"); //assigns add button to addButton variable
 const removeButton = document.querySelector(".remove-button"); //assigns remove button to removeButton variable
--- Portfolio/issues/Issue8.js
+++ Portfolio/issues/Issue8.js
@@ -1,11 +1,11 @@
 const button = document.getElementById("api"); //selecting button by element ID
-const body1 = document.getElementById('apiSection');
+const issue8Body = document.getElementById('apiSection');
 
 function createElement(element, text, name) { //function to create an element given its parameters of element type, text content, and id-name
   const created = document.createElement(element); //variable for the newly created element
   created.setAttribute("class", name); //sets created elements id = name parameter
   created.textContent = text; //sets text content to text parameter
-  body.appendChild(created); //appends to html <body>
+  issue8Body.appendChild(created); //appends to html <body>
   return created;
 }
 //adding a click event listener to add loading text when the button is fired..
@@ -20,7 +20,7 @@ function createElement(element, text, name) { //function to create an element gi
             return response.json(); //JSON formatted response of data
         })
         .then((data) => {
-            const result = createElement('p', JSON.stringify(data, null, 3), 'data')
+            const result = createElement('pre', JSON.stringify(data, null, 3), 'data')
             document.getElementById("apiSection").appendChild(result); //appending that text to the apiSection id element
         })
         .catch((error) => {
@@ -28,8 +28,9 @@ function createElement(element, text, name) { //function to create an element gi
             document.getElementById("apiSection").appendChild(errorResult); //If an error occurs, it will append a p element to the apiSection ID element
         })
         .finally(() => {
+            //Removes Loading... text once the promise is satisfied
+            const loading = document.querySelector(".loading");
             loading.remove();
-             //Removes Loading... text once the promise is satisfied
         })
          //disables the button after its been clicked once so that you cannot spam page with sw facts
          button.disabled = true;
