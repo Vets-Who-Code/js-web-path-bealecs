d759f491d29e905fe603d15a9291bbaf44b39071
bealecs <cliftonscottbeale@gmail.com>
2023-04-20 13:00:51 -0500

    issue 9 ready for review

--- src/index.tsx
+++ src/index.tsx
@@ -4,3 +4,18 @@ interface Object {
     value: string;
 }
 
+type myArr = Array<Object>;
+
+//Function iterating through number parameter times to create that many new array objects with random strings.
+function getArrNum(newArr: myArr, num: number) {
+    for(let i = 0; i < num; i++) {
+        newArr.push(Math.random().toString(36).substring(7));
+    }
+    return newArr;
+}
+
+const finalArr: myArr = []; //declaring a new variable with the type of the earlier created type, and making it an empty array
+
+const callFunc = getArrNum(finalArr, 10); //storing the function call to a variable
+
+console.log(callFunc); //console log
