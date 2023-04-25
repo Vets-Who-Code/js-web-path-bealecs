58e373c9378425d295e49745ff71606e8a20f4b1
bealecs <cliftonscottbeale@gmail.com>
2023-04-25 12:08:39 -0500

    added hamburger menu for tablet/mobile navigation

--- src/issues/eventListeners.js
+++ src/issues/eventListeners.js
@@ -4,6 +4,12 @@ const aboutButton = document.getElementById("aboutID");
 const contactButton = document.getElementById("contactID");
 const resumeButton = document.getElementById("resumeID");
 const logoButton = document.getElementById("logotitle");
+const hamburger = document.querySelector(".hamburger-menu");
+const dropDown = document.querySelector(".drop-down");
+const burgerProjectsButton = document.getElementById("hamburgerProjectsID");
+const burgerAboutButton = document.getElementById("hamburgerAboutID");
+const burgerContactButton = document.getElementById("hamburgerContactID");
+const burgerResumeButton = document.getElementById("hamburgerResumeID");
 
 //buttons for project section
 const pwpProject = document.getElementById("pwpSection");
@@ -17,25 +23,60 @@ const contactSection = document.getElementById("contactSection");
 const resumeSection = document.getElementById("resumeSection");
 const landingSection = document.getElementById("landingSection");
 
+//toggle dropdown function
+const toggleDropDown = () => {
+    hamburger.classList.toggle("active");
+    dropDown.classList.toggle("active");
+}
+//utilizes scrollIntoView
+const scrollIntoViewFunc = (section) => {
+    section.scrollIntoView({behavior: "smooth"});
+}
+
 //Event listeners being created
 logoButton.addEventListener('click', () => {
-    landingSection.scrollIntoView({behavior: "smooth"});
+    scrollIntoViewFunc(landingSection
+        );
 });
 
 projectsButton.addEventListener('click', () => {
-    projectsSection.scrollIntoView({behavior: "smooth"});
+    scrollIntoViewFunc(projectsSection);
 });
 
 aboutButton.addEventListener('click', () => {
-    aboutSection.scrollIntoView({behavior: "smooth"});
+    scrollIntoViewFunc(aboutSection);
 });
 
 contactButton.addEventListener('click', () => {
-    contactSection.scrollIntoView({behavior: "smooth"});
+    scrollIntoViewFunc(contactSection);
 });
 
 resumeButton.addEventListener('click', () => {
-    resumeSection.scrollIntoView({behavior: "smooth"});
+    scrollIntoViewFunc(resumeSection);
+});
+
+burgerProjectsButton.addEventListener('click', () => {
+    scrollIntoViewFunc(projectsSection);
+    toggleDropDown();
+});
+
+burgerAboutButton.addEventListener('click', () => {
+    scrollIntoViewFunc(aboutSection);
+    toggleDropDown();
+});
+
+burgerContactButton.addEventListener('click', () => {
+    scrollIntoViewFunc(contactSection);
+    toggleDropDown();
+});
+
+burgerResumeButton.addEventListener('click', () => {
+    scrollIntoViewFunc(resumeSection);
+    toggleDropDown();
+});
+
+hamburger.addEventListener('click', () => {
+    toggleDropDown();
 });
 
 //project section event listeners
