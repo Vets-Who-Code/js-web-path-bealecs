04f7171006e0c493b365fb4cd372626cca18071b
bealecs <cliftonscottbeale@gmail.com>
2023-05-16 16:35:58 -0500

    made css modular

--- app/Components/Hero.tsx
+++ app/Components/Hero.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './CSS_Modules/Hero.css'
+import HeroStyles from './CSS_Modules/Hero.module.css'
 import Image from "next/image";
 import 'font-awesome/css/font-awesome.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -8,18 +8,18 @@ import { faCode } from "@fortawesome/free-solid-svg-icons"
 export const HeroSection = () => {
 
     return (
-    <section className="landing-section" id="landingSection">
+    <section className={HeroStyles.landingSection} id="landingSection">
         <div>
-            <Image height={375} width={400} src="/pfp.webp" className="pfp" alt="Professional shot of Clif Beale with a transparent background"/>
+            <Image height={375} width={400} src="/pfp.webp" className={HeroStyles.pfp} alt="Professional shot of Clif Beale with a transparent background"/>
         </div>
-        <div className="landing-info">
-            <h2 className="name">I'm <span className="name2">Clif</span></h2>
-            <h3 className="typewriter-text"><span className="typewriter">Husband, Web Developer, Veteran, Gamer</span><span className="typewriter-cursor"></span></h3>
-            <a className="vwc" href="https://vetswhocode.io" target="_blank" rel="noopener noreferrer">#VetsWhoCode</a>
-            <div className="icons-flex">
-                <FontAwesomeIcon icon={faCode} width={50} className="icon" />
-                <FontAwesomeIcon icon={faCode} width={50} className="icon" />
-                <FontAwesomeIcon icon={faCode} width={50} className="icon" />
+        <div className={HeroStyles.landingInfo}>
+            <h2 className={HeroStyles.name}>I'm <span className={HeroStyles.name2}>Clif</span></h2>
+            <h3 className={HeroStyles.typewriterText}><span className={HeroStyles.typewriter}>Husband, Web Developer, Veteran, Gamer</span><span className={HeroStyles.typewriterCursor}></span></h3>
+            <a className={HeroStyles.vwc} href="https://vetswhocode.io" target="_blank" rel="noopener noreferrer">#VetsWhoCode</a>
+            <div className={HeroStyles.iconsFlex}>
+                <FontAwesomeIcon icon={faCode} width={50} className={HeroStyles.icon} />
+                <FontAwesomeIcon icon={faCode} width={50} className={HeroStyles.icon} />
+                <FontAwesomeIcon icon={faCode} width={50} className={HeroStyles.icon} />
             </div>
         </div>
     </section>
--- app/Components/Navigation.tsx
+++ app/Components/Navigation.tsx
@@ -1,32 +1,32 @@
 import React from "react"
-import "./CSS_Modules/Navigation.css";
+import NavigationStyles from "./CSS_Modules/Navigation.module.css";
 
 export const Navigation = () => {
 
     return (
         <>
-        <nav>
-            <h1 id="logotitle" className="logo-title"><span className="logo-letters">C</span>lif <span className="logo-letters">C</span>odes</h1>
+        <nav className={NavigationStyles.nav}>
+            <h1 id="logotitle" className={NavigationStyles.logoTitle}><span className={NavigationStyles.logoLetters}>C</span>lif <span className={NavigationStyles.logoLetters}>C</span>odes</h1>
             <ul>
-                <li className="nav-button" id="projectsID">Projects <i className="fa fa-handshake-o"></i></li>
-                <li className="nav-button" id="aboutID">About <i className="fa fa-male"></i></li>
-                <li className="nav-button" id="contactID">Contact <i className="fa fa-address-book-o"></i></li>
-                <li className="nav-button" id="resumeID">Resume <i className="fa fa-briefcase"></i></li>
-                <li className="themer" id="themer"></li>
+                <li className={NavigationStyles.navButton} id="projectsID">Projects</li>
+                <li className={NavigationStyles.navButton} id="aboutID">About</li>
+                <li className={NavigationStyles.navButton} id="contactID">Contact</li>
+                <li className={NavigationStyles.navButton} id="resumeID">Resume</li>
+                <li className={NavigationStyles.themer} id="themer"></li>
             </ul>
-            <div className="hamburger-menu">
-                <span className="hamburger-bar"></span>
-                <span className="hamburger-bar"></span>
-                <span className="hamburger-bar"></span>
+            <div className={NavigationStyles.hamburgerMenu}>
+                <span className={NavigationStyles.hamburgerBar}></span>
+                <span className={NavigationStyles.hamburgerBar}></span>
+                <span className={NavigationStyles.hamburgerBar}></span>
             </div>
         </nav>
 
-        <section className="drop-down">
+        <section className={NavigationStyles.dropDown}>
         <ul>
-            <li className="nav-button" id="hamburgerProjectsID">Projects</li>
-            <li className="nav-button" id="hamburgerAboutID">About</li>
-            <li className="nav-button" id="hamburgerContactID">Contact</li>
-            <li className="nav-button" id="hamburgerResumeID">Resume</li>
+            <li className={NavigationStyles.navButton} id="hamburgerProjectsID">Projects</li>
+            <li className={NavigationStyles.navButton} id="hamburgerAboutID">About</li>
+            <li className={NavigationStyles.navButton} id="hamburgerContactID">Contact</li>
+            <li className={NavigationStyles.navButton} id="hamburgerResumeID">Resume</li>
         </ul>
     </section>
     </>
